dd3b8eca3add791d4c0d6b076dfbd76a5345e3fd
Hims-21 <himansugami53054@gmail.com>
2023-08-10 17:29:07 +0530

    first commit

---
+++ MigrationDemo/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationDemo.DataModels;
+
+namespace MigrationDemo.ApplicationDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MigrationDemo/DataModels/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace MigrationDemo.DataModels
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
